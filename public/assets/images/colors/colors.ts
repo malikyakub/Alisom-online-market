99163af73b6c90a10453ba76fdf2d4adb87e2b4a
malik yakub <malikyakub@hotmail.com>
2025-04-04 13:11:56 +0300

    assets are added

---
+++ public/assets/images/colors/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+    primaryBlue: "#007BFF",
+    teal: "#17C3B2",
+    lightGray: "#F4F4F4",
+    navyBlue: "#1A2238",
+    mediumGray: "#A3A3A3",
+    white: "#FFFFFF",
+    darkGray: "#333333",
+    gray: "#666666",
+    alert: {
+        success: "#28A745",
+        warning: "#FFC107",
+        danger: "#DC3545",
+    },
+};
