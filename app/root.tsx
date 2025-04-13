ff83784674d40529fb12cc66e3dcc919d29ca2b6
Taatiko-teach <utaakow@gmail.com>
2025-04-13 21:05:24 +0300

    footer kusoo daray

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import Header from "components/Header";
+import Footer from "components/Footer";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -34,14 +35,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       {/* <body className="bg-[#007BFF] text-gray-900 font-sans min-h-screen flex flex-col"> */}
       <body className="text-gray-900 font-sans min-h-screen flex flex-col">
-        <div className="w-full max-w-[1170px] mx-auto px-4 sm:px-6 sticky top-0 z-50 bg-white/70 backdrop-blur-md">
+        {/* <div className="w-full max-w-[1170px] mx-auto px-4 sm:px-6 sticky top-0 z-50 bg-white/70 backdrop-blur-md">
           <Header />
-        </div>
+        </div> */}
 
         <main className="flex-1 w-full max-w-[1170px] mx-auto px-4 sm:px-6 py-6">
           {children}
         </main>
-
+<Footer/>
         <ScrollRestoration />
         <Scripts />
       </body>
