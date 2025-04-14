9e7e28beee4f2379575807907af3ea29fb7edc88
Annab-mohamet <anablove75@gmail.com>
2025-04-15 00:03:29 +0300

    Add ProductCard2 component with badge

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,5 @@
+
+
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import CategoryCard from "components/CategoryCard";
@@ -8,6 +10,7 @@ import { CgSmartphone } from "react-icons/cg";
 import InformationCard from "components/InformationCard";
 import { CiDollar } from "react-icons/ci";
 import TotalCard from "components/TotalCard";
+import ProductCard2 from "components/ProductCard2";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -29,12 +32,15 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <div className="min-h-screen p-5 flex flex-col gap-10">
+      
+      {/* Categories */}
       <div className="flex flex-row items-center gap-4">
         <CategoryCard icon={RiComputerFill} text="Computers" />
         <CategoryCard icon={CgSmartphone} text="SmartPhones" />
       </div>
 
-      <div className="flex flex-row flex-wrap items-center  gap-4">
+      {/* Product Cards */}
+      <div className="flex flex-row flex-wrap items-center gap-4">
         <ProductCard
           image="https://i.pinimg.com/736x/ee/3e/4c/ee3e4c0c619248199d6cef77294db156.jpg"
           title="laptop"
@@ -49,6 +55,7 @@ export default function Home() {
         />
       </div>
 
+      {/* Information Cards */}
       <div className="flex flex-row flex-wrap items-center gap-4">
         <InformationCard
           icon={IoStorefrontSharp}
@@ -60,16 +67,54 @@ export default function Home() {
         <InformationCard
           icon={CiDollar}
           value="33k"
-          label="Monthly Produduct Sale"
+          label="Monthly Product Sale"
           iconBgColor="#1A2238"
           textColor="#1A2238"
         />
       </div>
-      <div className="flex ">
+
+      {/* TotalCard */}
+      <div className="flex">
         <div className="w-full md:w-[320px]">
           <TotalCard />
         </div>
       </div>
+
+      {/* ProductCard2 */}
+      
+      <div>
+  <div className="mt-6">
+    <ProductCard2
+      image="https://i.pinimg.com/474x/69/95/67/699567b89b880a8ea5cfd3ccb9ee8071.jpg"
+      name="CANON EOS DSLR Camera"
+      price={360}
+      rating={4}
+      reviews={95}
+    />
+  </div>
+  <div className="mt-6">
+    <ProductCard2
+      image="https://i.pinimg.com/736x/36/70/5e/36705ec9917ce4b400898fd8bd53eeda.jpg"
+      name="ASUS FHD Gaming Laptop"
+      price={700}
+      rating={5}
+      reviews={325}
+    />
+  </div>
+      <div>
+        <div className="mt-6">
+    <ProductCard2
+      image="https://i.pinimg.com/736x/a2/46/89/a24689a207646ccfb0b878a007f115d8.jpg"
+      name="Curology Product Set"
+      price={500}
+      rating={5}
+      reviews={145}
+    />
+  </div>
+      </div>
+       
+</div>
+
     </div>
   );
 }
