25c9b9bffb832cfd9cff13c49f4c6b38b4be795f
Mohamoud Adam <mohamoudadam2000@gmail.com>
2025-04-16 19:23:08 +0300

    i created UserAccount route page

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("Login", "routes/Login.tsx")
-    ,
-    route("Signup", "routes/Signup.tsx")
+    route("Login", "routes/Login.tsx"),
+    route("Signup", "routes/Signup.tsx"),
+    route("Useraccount", "routes/UserAccount.tsx"),
+    
 ] satisfies RouteConfig;
