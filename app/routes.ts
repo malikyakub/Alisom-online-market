fa8a8e22316f9d7d30e8bc90e817787227dc6e4c
malik yakub <malikyakub@hotmail.com>
2025-05-07 19:29:38 +0300

    some fixes and making the page responsive

--- app/routes.ts
+++ app/routes.ts
@@ -7,12 +7,11 @@ export default [
   route("Contact", "routes/Contact.tsx"),
   route("User", "routes/User/_layout.tsx", {}, [
     route("Account", "routes/User/Account.tsx"),
-    route("checkoutpage", "routes/User/checkoutpage.tsx"),
+    route("Checkout", "routes/User/Checkout.tsx"),
   ]),
-  
+
   route("Admin", "routes/Admin/_layout.tsx", {}, [
     route("Dashboard", "routes/Admin/Dashboard.tsx"),
   ]),
   route("*", "routes/NotFound.tsx"),
-  
 ] satisfies RouteConfig;
