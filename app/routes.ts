b0eb02c2f14cf8b14c4908a2ba534649cc6ed656
malik yakub <malikyakub@hotmail.com>
2025-05-29 14:05:28 +0300

    the reset password route added

--- app/routes.ts
+++ app/routes.ts
@@ -6,6 +6,7 @@ export default [
   route("Login", "routes/Login.tsx"),
   route("Signup", "routes/Signup.tsx"),
   route("forgot-password", "routes/ForgorPassword.tsx"),
+  route("reset-password", "routes/ResetPassword.tsx"),
   route("Contact", "routes/Contact.tsx"),
   route("About", "routes/About.tsx"),
   route("privacy-policy", "routes/PrivacyPolicy.tsx"),
